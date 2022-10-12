d1a59efd8aeaa9db24e4a5abd21d82843142ffb3
Vealer <108185061+Vealer@users.noreply.github.com>
2022-10-12 13:15:20 +0200

    14 files modified

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
+  cards=[{name:'HTML', src:'assets/html.png'}, {name:'CSS', src:'assets/css.png'},{name:'JavaScript', src:'assets/js.png'},{name:'Git', src:'assets/git.png'},{name:'API', src:'assets/api.png'},{name:'Angular', src:'assets/angular.png'},{name:'SCRUM', src:'assets/scrum.png'},];
 
   constructor() { }
 
