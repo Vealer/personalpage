64dc1b40d8221d0f57fc88f1caf4f61672bb051d
Vealer <108185061+Vealer@users.noreply.github.com>
2022-10-12 14:55:13 +0200

    5 files modified

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  journey=[
+  {src:'/assets/walking.png', title:'The JOURNEY began', text:'I started programming as a hobby when I was 14 years old and I still enjoy programming immensely.'},
+  {src:'/assets/heart.png', title:'Become pleasures', text:'I really enjoy developing software. For this reason I want to turn my hobby into my profession.'},
+  {src:'/assets/lupe.png', title:'Search for more', text:'I realized I wanted more. I wanted to turn my hobby into my job. So I went looking and found the Developer Academy.'},
+  {src:'/assets/takeoff.png', title:'Ready to takeoff', text:'I am now looking for new challenges to work as a Front End developer.'},
+]
   constructor() { }
 
   ngOnInit(): void {
