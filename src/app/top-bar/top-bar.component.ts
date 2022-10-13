b4abcb38083b0cea33d0d63261d8138ad98761e5
Vealer <108185061+Vealer@users.noreply.github.com>
2022-10-13 14:49:31 +0200

    4 files modified

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -3,12 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+  styleUrls: ['./top-bar.component.css'],
 })
 export class TopBarComponent {
+  constructor( ) { 
+    this.change();
+  }
 
+  english: boolean = true;
+  aboutme: string = '';
+  skills: string = '';
+  contact: string = '';
+  text: string = '';
+
+  change() {
+    this.english = !this.english;
+    let en: boolean = this.english;
+    this.text = en ? 'English' : 'Deutsch';
+    this.aboutme = en ? 'About Me' : 'Über mich';
+    this.skills = en ? 'Skills' : 'Fähigkeiten';
+    this.contact = en ? 'Contact me' : 'Kontakt';
+  }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
