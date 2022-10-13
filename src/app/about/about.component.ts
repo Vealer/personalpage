4de4771b93df8e524836ef3650f5b8f604546b62
Vealer <108185061+Vealer@users.noreply.github.com>
2022-10-13 12:09:25 +0200

    5 files modified, 3 deleted

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   journey=[
-  {src:'/assets/walking.png', title:'The JOURNEY began', text:'I started programming as a hobby when I was 14 years old and I still enjoy programming immensely.'},
+  {src:'/assets/walking.png', title:'The JOURNEY began', text:'I started programming as a hobby a few years ago and I still enjoy programming immensely.'},
   {src:'/assets/heart.png', title:'Become pleasures', text:'I really enjoy developing software. For this reason I want to turn my hobby into my profession.'},
   {src:'/assets/lupe.png', title:'Search for more', text:'I realized I wanted more. I wanted to turn my hobby into my job. So I went looking and found the Developer Academy.'},
-  {src:'/assets/takeoff.png', title:'Ready to takeoff', text:'I am now looking for new challenges to work as a Front End developer.'},
+  {src:'/assets/takeoff.png', title:'Ready to takeoff', text:'I am now looking for new challenges to work as a Front End developer or build software with JavaScript.'},
 ]
   constructor() { }
 
