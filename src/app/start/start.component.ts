a0150f13fad6731c1303b7c5bc165cc5bdd47c07
Vealer <108185061+Vealer@users.noreply.github.com>
2022-10-13 15:15:06 +0200

    1 file modified

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-start',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
   
+@Input()
+english: boolean = false;
 
   constructor() { }
 
