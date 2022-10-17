2a04a56f8c87b7ed41ac92e5a8d9b89ccb365f83
Vealer <108185061+Vealer@users.noreply.github.com>
2022-10-17 08:58:54 +0200

    3 files modified

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -3,16 +3,33 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
-  styleUrls: ['./start.component.css']
+  styleUrls: ['./start.component.css'],
 })
 export class StartComponent implements OnInit {
-  
-@Input()
-english: boolean = false;
+  @Input()
+  english: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.changeText();
   }
 
+  ngOnInit(): void {}
+
+  text1: string = '';
+  text2: string = '';
+  text3: string = '';
+  text4: string = '';
+
+  changeText() {
+    let en: boolean = this.english;
+    this.text1 = en ? `Hi! I'm Val` : `Hi! Ich bin Val`;
+    this.text2 = en ? `a w` : `ein W`;
+    this.text3 = en ? `develop` : `Entwickl`;
+    this.text4 = en ? `Frontend developer` : `Frontend Entwickler`;
+    
+  }
+
+  ngOnChanges() {
+    this.changeText();
+  }
 }
