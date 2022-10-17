c80e0cf8f40829e3360049edb2fff894ce9c2b4d
Vealer <108185061+Vealer@users.noreply.github.com>
2022-10-17 10:33:52 +0200

    3 files modified

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  @Input()
+  english: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  text1: string = '';
+  text2: string = '';
+  text3: string = '';
+  text4: string = '';
+
+  changeText() {
+    let en: boolean = this.english;
+    this.text1 = en ? `Contact me` : `Kontakt`;
+    this.text2 = en ? `Get in touch via the form below, or by emailing` : `Kontaktieren Sie mich über das unten stehende Formular oder per E-Mail: `;
+    this.text3 = en ? `Message` : `Nachricht`;
+    this.text4 = en ? `Send message` : `Nachricht senden`;
+
+    
+  }
+
+  ngOnChanges() {
+    this.changeText();
   }
 
 }
