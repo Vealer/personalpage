d2ed8ca233e0a696b306de9a2601f66fdbb9210b
Vealer <108185061+Vealer@users.noreply.github.com>
2022-10-17 09:28:47 +0200

    3 files modified

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  @Input()
+  english: boolean = false;
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  text1: string = '';
+  text2: string = '';
+  text3: string = '';
+  text4: string = '';
+  text5: string = '';
+
+  changeText() {
+    let en: boolean = this.english;
+    this.text1 = en ? `My` : `Mein`;
+    this.text2 = en ? `A jump & run game` : `Ein jump & run Spiel`;
+    this.text3 = en ? `A canban group project` : `Eine Kanban Gruppenarbeit`;
+    this.text4 = en ? `A socialmedia clone` : `Eine Instagram Kopie`;
+    this.text5 = en ? `A game, written with Angular` : `Ein Kartenspiel mit Angular geschrieben`;
+    
+  }
+
+  ngOnChanges() {
+    this.changeText();
   }
 
 }
