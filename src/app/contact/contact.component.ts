96a6d4c16faa5b9348cc3a3dfa4f1ce0f73a637a
Vealer <108185061+Vealer@users.noreply.github.com>
2022-10-19 12:01:15 +0200

    2 files modified

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,32 +7,30 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  @Input()
   english: boolean = false;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
   text1: string = '';
   text2: string = '';
   text3: string = '';
   text4: string = '';
 
+  constructor(private dataService: DataService) { 
+    this.changeText();
+  }
+
+  ngOnInit() {
+    this.dataService
+      .langChanged
+      .subscribe(() => {
+        this.english = this.dataService.get();
+        this.changeText();
+      });
+  }
+  
   changeText() {
     let en: boolean = this.english;
     this.text1 = en ? `Contact me` : `Kontakt`;
     this.text2 = en ? `Get in touch via the form below, or by emailing` : `Kontaktieren Sie mich über das unten stehende Formular oder per E-Mail: `;
     this.text3 = en ? `Message` : `Nachricht`;
     this.text4 = en ? `Send message` : `Nachricht senden`;
-
-    
   }
-
-  ngOnChanges() {
-    this.changeText();
-  }
-
 }
